1d1483873337a3f415f93a0c02d5186def8cd78d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-01-05 02:53:01 +0500

    project has been finished

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +128,9 @@ AUTH_PASSWORD_VALIDATORS = [
 SWAGGER_SETTINGS = {
     "DEFAULT_AUTO_SCHEMA_CLASS": "drf_yasg.inspectors.SwaggerAutoSchema",
 }
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles') 
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
